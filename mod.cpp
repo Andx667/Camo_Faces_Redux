e3973b63cb8eb6f533af54f4e98416685d212e81
Andy <andx667@protonmail.com>
2025-12-12 14:14:26 +0100

    chance script

--- mod.cpp
+++ mod.cpp
@@ -1,6 +1,6 @@
 author = "Andx";
 name = "Camo Faces Redux";
-description = "Camo Faces Redux - Version 0.0.0";
+description = "Camo Faces Redux - Version {version}";
 overview = "Camo Faces Redux is a Rewrite of the Camo Faces Mod, which enables Players to camouflage their faces in Arma3";
 overviewPicture = "logo_cfr_ca.paa";
 picture = "logo_cfr_ca.paa";
